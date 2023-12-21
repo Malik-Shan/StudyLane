bc275629036900e515351d6fd09e9b64b39a786b
Malik-Shan <60312975+Malik-Shan@users.noreply.github.com>
2023-12-22 00:59:08 +0500

    blog section

--- src/content/subjects/calculus-and-analytic-geometry-s1/second.md
+++ src/content/subjects/calculus-and-analytic-geometry-s1/second.md
@@ -1,12 +1,12 @@
 ---
-title: 'First lecture heading here.'
-date: '2023/12/21'
+title: '2nd lecture heading here.'
+date: '2023/12/22'
 tag:
      - 'first'
      - 'lecture'
 subject: 'english'
 semester: 'S1'
-type: 'lecture'
+type: 'blog'
 ---
 [link](ssww.com./)
 test value here man what do you thing ima gonna do man
--- src/js/util.js
+++ src/js/util.js
@@ -8,3 +8,32 @@ export function slugify(text){
     .replace(/^-+/, '')
     .replace(/-+$/, '');
 }
+export function formatAllPosts(posts, {
+    filterOutDraft = true,
+    sortByDate = true,
+    limit = undefined,
+} = {}){
+    const filteredPosts = posts.reduce((acc, post) => {
+        const {Draft} = post.data;
+        
+        //filterOutDrafts if true
+        if(filterOutDraft && Draft) return acc;
+
+        //add game to acc
+        acc.push(post)
+        return acc;
+    }, [])
+
+    // sortyByDate or randomize
+    if(sortByDate){
+        filteredPosts.sort((a, b) => new Date(b.data.date) - new Date(a.data.date))
+    } else {
+        filteredPosts.sort(() => Math.random() - .5)
+    }
+
+    // limit if number is passed
+    if(typeof limit === "number"){
+        return filteredPosts.slice(0 , limit);
+    }
+    return filteredPosts;
+}
