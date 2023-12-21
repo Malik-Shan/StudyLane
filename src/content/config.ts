8957a694e37513e72c7172351eb066ab3524c14e
Malik-Shan <60312975+Malik-Shan@users.noreply.github.com>
2023-12-21 21:45:10 +0500

    blogs

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,10 +1,8 @@
 import {defineCollection} from 'astro:content';
 
 const subjectsCollection = defineCollection({});
-const calc_s1 = defineCollection({});
 
 export const collections = {
   'subjects' : subjectsCollection,
-  'calculus-and-analytic-geometry-s1' : calc_s1,
 }
 
