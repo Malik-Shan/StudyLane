e20013c91785cff763ecf8a57c2e6e881538ef14
Malik-Shan <60312975+Malik-Shan@users.noreply.github.com>
2024-01-02 22:33:14 +0500

    few responsive and admin page update

--- src/pages/api/auth/registerUser.ts
+++ src/pages/api/auth/registerUser.ts
@@ -38,8 +38,10 @@ export const POST: APIRoute = async ({request}) => {
 
   const customClaims = {};
   const rolesMap = roles.split(',');
+  let rolesHTML = ''
   rolesMap.map((r) => {
-    customClaims[r] = true
+    rolesHTML += `<span class='role'>${r}</span>`;
+    customClaims[r] = true;
   })
   let uid;
   try {
@@ -63,20 +65,26 @@ export const POST: APIRoute = async ({request}) => {
     })
   }
 const html = `
-				<div class='user' data-id={u.uid} id='uid-${uid}'}>
-					<p>${name}</p>
-					<p>${email}</p>
-					<p>${roles}</p>
-					<button class='secondaryBtn p-1 px-2 rounded-md border'><i class="fa-solid fa-pen-to-square"></i></button>
-					<button class='dangerBtn p-1 px-2 rounded-md border'
-						hx-delete='/api/auth/del_user'
-						hx-swap='delete swap:400ms'
-						hx-headers=''{"uid":"${uid}"}'
-						hx-target='#uid-${uid}'
-						hx-trigger='click'
-						hx-confirm='Are you sure?'
-						type='button'><i class="fa-solid fa-trash"></i></button>
-				</div>
+    <div class='user' data-id='uid' id='uid-${uid}'>
+      <div class='roles'>
+	${rolesHTML}
+      </div>
+      <p class='name'><i class="fa-solid fa-user"></i>${name}</p>
+
+      <p class='email'><i class="fa-solid fa-envelope"></i>${email}</p>
+
+      <button class='secondaryBtn p-1 px-2 rounded-md border'><i class="fa-solid fa-pen-to-square"></i></button>
+
+      <button class='dangerBtn p-1 px-2 rounded-md border'
+      hx-delete='/api/auth/del_user'
+      hx-swap='delete swap:400ms'
+	hx-headers='{"uid":"${uid}"}'
+	hx-target='#uid-${uid}'
+      hx-trigger='click'
+      hx-confirm='Are you sure?'
+      type='button'><i class="fa-solid fa-trash"></i></button>
+
+    </div>
 `
   return new Response( html ,{
     status:200,
