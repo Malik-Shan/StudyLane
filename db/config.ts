1107cf7fa675347dd63505e1da4401f246a1bce5
Malik-Shan <60312975+Malik-Shan@users.noreply.github.com>
2024-04-25 17:17:49 +0500

    db update

--- db/config.ts
+++ db/config.ts
@@ -7,7 +7,14 @@ const Data = defineTable({
     s: column.text(),
   }
 })
+const Courses = defineTable({
+  columns: {
+    id: column.number({primaryKey: true}),
+    name: column.text(),
+    duration: column.number(),
+  }
+})
 
 export default defineDb({
-  tables: {Data},
+  tables: {Data, Courses},
 })
