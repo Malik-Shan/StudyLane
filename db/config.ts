c83f51308e102c49fb39ec844cc0050b7cc444ea
Malik-Shan <60312975+Malik-Shan@users.noreply.github.com>
2024-04-26 05:17:03 +0500

    s

--- db/config.ts
+++ db/config.ts
@@ -1,10 +1,22 @@
+import { reference } from 'astro:content';
 import { column, defineDb, defineTable } from 'astro:db';
 
+const Subjects = defineTable({
+  columns: {
+    sid: column.text({primaryKey: true}),
+    sname: column.text({optional: false}),
+    ssname: column.text({optional: false}),
+    simg: column.text({optional: false}),
+  },
+  indexes: [
+    {on: ["sid"], unique: true},
+  ]
+})
 const Data = defineTable({
   columns: {
     id: column.number({primaryKey: true}),
     a: column.text(),
-    s: column.text(),
+    s: column.text({references: () => Subjects.columns.sid}),
   }
 })
 const Courses = defineTable({
@@ -17,5 +29,5 @@ const Courses = defineTable({
 })
 
 export default defineDb({
-  tables: {Data, Courses},
+  tables: {Subjects,Data, Courses},
 })
