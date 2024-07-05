e56fd72041e0fb6eb1a8ffdc1760c0c02031ea0c
Malik-Shan <60312975+Malik-Shan@users.noreply.github.com>
2024-07-06 02:02:19 +0500

    audit page update ui

--- src/pages/api/db/create.ts
+++ src/pages/api/db/create.ts
@@ -6,12 +6,12 @@ import { db,Audits as zAudits } from 'astro:db';
 export const POST:APIRoute = async ({request}) => {
   const formData = await request.formData();
   const l = formData.get("links")?.toString().replace(/\r?\n/g, '\\n');
-  const text = formData.get("links")?.toString()  
+  const text = l.replace(/\\n/g, '<br>');  
   const d = new Date().getTime();
   const r = await db.insert(zAudits).values({date:d,links:l});
   const html = `
     <div class="card" id='card-${r.lastInsertRowid}'>
-    <button hx-delete='/api/db/delete/${r.lastInsertRowid}' hx-swap="outerHTML" hx-target='#card-${r.lastInsertRowid}'>Delete</button>
+    <button id='del-btn' hx-delete='/api/db/delete/${r.lastInsertRowid}' hx-swap="outerHTML" hx-indicator='#card-${r.lastInsertRowid}' hx-target='#card-${r.lastInsertRowid}'>Delete</button>
     <h2>${new Date(d).toLocaleString('en-US',{timeZone:'Asia/Karachi'})}</h2>
     <p>${text}</p>
     </div>
