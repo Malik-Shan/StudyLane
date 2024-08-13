7c9655bcfada55ef1f5fe46b148cdf34b6e8c63f
Malik-Shan <60312975+Malik-Shan@users.noreply.github.com>
2024-08-13 19:30:32 +0500

    update editor

--- src/pages/api/db/create.ts
+++ src/pages/api/db/create.ts
@@ -5,15 +5,14 @@ import { db,Audits as zAudits } from 'astro:db';
 
 export const POST:APIRoute = async ({request}) => {
   const formData = await request.formData();
-  const l = formData.get("links")?.toString().replace(/\r?\n/g, '\\n');
-  const text = l.replace(/\\n/g, '<br>');  
+  const l = formData.get("links")?.toString()
   const d = new Date().getTime();
   const r = await db.insert(zAudits).values({date:d,links:l});
   const html = `
     <div class="card" id='card-${r.lastInsertRowid}'>
     <button id='del-btn' hx-delete='/api/db/delete/${r.lastInsertRowid}' hx-swap="outerHTML" hx-indicator='#card-${r.lastInsertRowid}' hx-target='#card-${r.lastInsertRowid}'>Delete</button>
     <h2>${new Date(d).toLocaleString('en-US',{timeZone:'Asia/Karachi'})}</h2>
-    <p>${text}</p>
+    <p>${l}</p>
     </div>
   `;
   if(r.rowsAffected === 1){
