3d7eb8b7d3c82a814a7de6c64ee2c92e1d13c473
Malik-Shan <60312975+Malik-Shan@users.noreply.github.com>
2024-11-26 05:08:53 +0500

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,22 +5,19 @@ import react from "@astrojs/react";
 import markdoc from "@astrojs/markdoc";
 import keystatic from "@keystatic/astro";
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
   prefetch: {
     prefetchAll: false,
     defaultStrategy: "hover",
   },
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    react(),
-    markdoc({
-      allowHTML: true,
-    }),
-    keystatic(),
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: false,
+  }), react(), markdoc({
+    allowHTML: true,
+  }), keystatic(), svelte()],
   output: "hybrid",
   adapter: vercel(),
 });
