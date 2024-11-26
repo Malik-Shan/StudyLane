e48d18c6fe7dc79666f5c1e0fa69870fa7a25885
Malik-Shan <60312975+Malik-Shan@users.noreply.github.com>
2024-11-26 05:08:53 +0500

    update

---
+++ src/components/Svelte/Increment.svelte
@@ -0,0 +1,10 @@
+<script>
+	let i = $state([1, 2, 3, 4]);
+	let total = $derived(i.reduce((acc, n) => acc + n, 0));
+	function push() {
+		i.push(i.length + 1);
+	}
+</script>
+
+<p>{total}</p>
+<button onclick={push}>Click</button>
