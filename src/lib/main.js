097fadde1f00825b3704a18f7446d48b084450b3
Malik-Shan <60312975+Malik-Shan@users.noreply.github.com>
2024-12-02 19:58:12 +0500

    copy and other update

--- src/lib/main.js
+++ src/lib/main.js
@@ -9,3 +9,23 @@ function showMenu() {
     }
   });
 }
+function setupCopyCode() {
+  if (!navigator.clipboard) return;
+  const allPres = document.querySelectorAll("pre");
+  if (allPres.length === 0) return;
+  allPres.forEach((pre) => {
+    const button = document.createElement("button");
+    button.classList.add("copybtn");
+    button.innerHTML = "Copy";
+    pre.prepend(button);
+    button.addEventListener("click", () => {
+      const code = pre.querySelector("code");
+      button.innerHTML = "COPIED";
+      navigator.clipboard.writeText(code.innerText);
+      setTimeout(() => {
+        button.innerHTML = "Copy";
+      }, 800);
+    });
+  });
+}
+setupCopyCode();
